2622994c41e05ee810f4d24e6019477aae98053b
ukirsche <ukirschenmann@gmx.de>
2018-01-17 15:45:57 +0100

    Issue #10: Scrollview erstellt mit dynamischer Zugabe von Textbausteinen

---
+++ Scripts/PopulateVertical.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulateVertical : MonoBehaviour {
+
+	public GameObject prefabDialogText;
+	private string test="blalbalbal, ist ja wunder bar\n was hier so alles abgeht,\n ihr arschwichser";
+	public int numCount;
+
+	// Use this for initialization
+	void Start () {
+		Populate ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void Populate(){
+		for (int i = 0; i < numCount; i++) {
+			addDialogText (test);
+		}
+	}
+
+	/// <summary>
+	/// Adds the dialog text to the Vertical Layout Group
+	/// </summary>
+	public void addDialogText(string dialogString){
+		GameObject newDialog;
+		newDialog = Instantiate (prefabDialogText, transform); //ensures that
+		newDialog.GetComponent<Text> ().text = dialogString;
+	}
+}
