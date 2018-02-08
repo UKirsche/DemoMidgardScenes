36192cf297c480bb8eca9ff15104db26fbcc4294
ukirsche <ukirschenmann@gmx.de>
2018-02-08 16:31:12 +0100

    Capsule Collider skript nur mit Capsule Collider ausführen

--- Scripts/CapsuleColiderInfo.cs
+++ Scripts/CapsuleColiderInfo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(CapsuleCollider))]
 public class CapsuleColiderInfo : MonoBehaviour {
 	public GameObject dialogView;
 	private Infopaket dialogPackage;
