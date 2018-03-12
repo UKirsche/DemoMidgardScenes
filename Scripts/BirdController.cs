0215e69633cd9ea26d83276a43c3af1f3f266a3f
ukirsche <ukirschenmann@gmx.de>
2018-03-12 23:51:14 +0100

    Issue #27 Vögel Beta fertig

---
+++ Scripts/BirdController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Bird controller: erzeugt Anzahl von Vögeln mit verschiedener Flugrichtung und Geschwindigkeit. 
+/// </summary>
+public class BirdController : MonoBehaviour {
+
+	enum SpawnQuadrant 
+	{
+		One,
+		Two,
+		Three,
+		Four
+	}
+
+	public GameObject bird;
+	public GameObject birdsArea;
+	public int birdNumber;
+	public int waitBird=10;
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (BirdWave ());
+	}
+
+
+	IEnumerator BirdWave () {
+		for (int i = 0; i < birdNumber; i++) {
+			Instantiate (bird, BirdSpawnPoint(), Quaternion.identity);
+			yield return new WaitForSeconds (waitBird);
+		}
+	}
+
+
+	private Vector3 BirdSpawnPoint(){
+		BoxCollider birdCollider = birdsArea.GetComponent<BoxCollider> ();
+		Bounds birdBounds = birdCollider.bounds;
+
+		//Choose upper quadrant of bounding box for spawn:
+		SpawnQuadrant spQuad = GetRandowmSpawnQuadrant();
+		return GetSpawnPoint (birdBounds, spQuad);
+
+	}
+
+
+	private SpawnQuadrant GetRandowmSpawnQuadrant(){
+		int randVal = Random.Range (1, 5);
+		return (SpawnQuadrant)randVal;
+	}
+
+	private Vector3 GetSpawnPoint(Bounds bounds, SpawnQuadrant spQ){
+		Vector3 spPoint = Vector3.zero;
+		Vector3 addVector = spPoint;
+		int extX = Mathf.RoundToInt (bounds.extents.x);
+		int extY = Mathf.RoundToInt (bounds.extents.y);
+		int extZ = Mathf.RoundToInt (bounds.extents.z);
+
+		switch (spQ) {
+		case SpawnQuadrant.One:
+			addVector.y += Random.Range (1, extY);
+			addVector.x -= Random.Range (1, extX);
+			addVector.z += Random.Range (1, extZ);
+
+			break;
+		case SpawnQuadrant.Two:
+			addVector.y += Random.Range (1, extY);
+			addVector.x -= Random.Range (1, extX);
+			addVector.z -= Random.Range (1, extZ);
+			break;
+		case SpawnQuadrant.Three:
+			addVector.y += Random.Range (1, extY);
+			addVector.x += Random.Range (1, extX);
+			addVector.z -= Random.Range (1, extZ);
+			break;
+		case SpawnQuadrant.Four:
+			addVector.y += Random.Range (1, extY);
+			addVector.x += Random.Range (1, extX);
+			addVector.z += Random.Range (1, extZ);
+			break;
+		default:
+			break;
+		}
+		spPoint = bounds.center + addVector;
+
+		return spPoint;
+	}
+}
---
+++ Scripts/BirdFlight.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Bird flight: Bird flies
+/// </summary>
+public class BirdFlight : MonoBehaviour {
+
+	private Rigidbody rb;
+	public int speed = 10;
+	private string voegelTag = "birdBox";
+	private Bounds birdBounds;
+	private Compass birdCompass;
+
+
+	struct Compass
+	{
+		public float up,down,north,south,east,west;
+	}
+
+	void Start () {
+
+		GameObject voegelGO = GameObject.FindGameObjectWithTag (voegelTag);
+		BoxCollider birdCollider = voegelGO.GetComponent<BoxCollider> ();
+		birdBounds = birdCollider.bounds;
+		birdCompass = FillCompass ();
+		rb = GetComponent<Rigidbody>();
+		Vector3 flightDir = Random.insideUnitSphere;
+		Vector3 rotTowards = this.transform.position + flightDir;
+		transform.LookAt(rotTowards);
+		rb.velocity = flightDir * speed;
+	}
+
+	void FixedUpdate(){
+		ReflectDirection ();
+	}
+
+	/// <summary>
+	/// Reflects the velocity vector of rigidbody.
+	/// </summary>
+	void ReflectDirection ()
+	{
+		bool outUp = OutOfBounds (birdCompass.up, this.transform.position.y, true);
+		bool outDown = OutOfBounds (birdCompass.down, this.transform.position.y, false);
+		bool outNorth = OutOfBounds (birdCompass.north, this.transform.position.z, true);
+		bool outSouth = OutOfBounds (birdCompass.south, this.transform.position.z, false);
+		bool outEast = OutOfBounds (birdCompass.east, this.transform.position.x, true);
+		bool outWest = OutOfBounds (birdCompass.west, this.transform.position.x, false);
+
+		Vector3 reflectVelocity = rb.velocity;
+
+		if (outUp || outDown) {
+			reflectVelocity.y *= -1;
+		} else if (outNorth || outSouth) {
+			reflectVelocity.z *= -1;
+		} else if (outEast || outWest) {
+			reflectVelocity.x *= -1;	
+		}
+
+		rb.velocity = reflectVelocity;
+
+	}
+
+	/// <summary>
+	/// Outs the of bounds. greater means: out of bounds when valuePos > valRef
+	/// </summary>
+	/// <returns><c>true</c>, if of bounds was outed, <c>false</c> otherwise.</returns>
+	/// <param name="valRef">Value reference.</param>
+	/// <param name="valPos">Value position.</param>
+	/// <param name="bigger">If set to <c>true</c> greater.</param>
+	private bool OutOfBounds(float valRef, float valPos , bool greater){
+		bool retVal = false;
+		if (greater) {
+			retVal = valPos > valRef;
+		} else {
+			retVal = valPos < valRef;
+		}
+
+		return retVal;
+	}
+
+
+	private Compass FillCompass(){
+		Compass birdCompass = new Compass ();
+		birdCompass.up = birdBounds.center.y + birdBounds.extents.y;
+		birdCompass.down = birdBounds.center.y - birdBounds.extents.y;
+		birdCompass.north = birdBounds.center.z + birdBounds.extents.z;
+		birdCompass.south = birdBounds.center.z - birdBounds.extents.z;
+		birdCompass.east = birdBounds.center.x + birdBounds.extents.x;
+		birdCompass.west = birdBounds.center.x + birdBounds.extents.x;
+
+		return birdCompass;
+	}
+}
