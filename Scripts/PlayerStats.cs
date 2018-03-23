7c53bc91878dbe18cab865d7ffdee1f7766a277f
ukirsche <ukirschenmann@gmx.de>
2018-03-23 14:58:25 +0100

    Issue #34: Statische Klasse für Interszenen-Speicherung

---
+++ Scripts/PlayerStats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+/// <summary>
+/// Player stats: Store data between Sences
+/// </summary>
+public static class PlayerStats
+{
+	public enum Scenes{
+		Cambryg,
+		CaveOsrick,
+		CaveGirls,
+		Library
+	}
+	private static Scenes lastScene;
+
+	public static Scenes LastScene 
+	{
+		get 
+		{
+			return lastScene;
+		}
+		set 
+		{
+			lastScene = value;
+		}
+	}
+
+}
