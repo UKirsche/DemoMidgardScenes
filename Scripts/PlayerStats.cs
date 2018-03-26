f2c4aa84719abb138813df3d61a5ec00878e11bd
ukirsche <ukirschenmann@gmx.de>
2018-03-26 10:32:34 +0200

    Issue #36: Paralles gehen und reden für alle!

--- Scripts/PlayerStats.cs
+++ Scripts/PlayerStats.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 /// </summary>
 public static class PlayerStats
 {
-	public static readonly Vector3 startMidgard = new Vector3(27,1,183);
+	public static readonly Vector3 startMidgard = new Vector3(110,1,56);
 
 	private static Vector3 beamPosition;
 	public static Vector3 BeamPosition 
