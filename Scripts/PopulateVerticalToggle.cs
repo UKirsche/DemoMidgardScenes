e1e186ad308a46f252e5c64a54aacb481a16b0cf
ukirsche <ukirschenmann@gmx.de>
2018-05-16 16:16:48 +0200

    Issue #45: Refactor und neue Klasse

---
+++ Scripts/PopulateVerticalToggle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulateVerticalToggle : PopulateVertical {
+
+	private ToggleGroup toggleGroup;
+	private List<Toggle> toggleButtons;
+
+	// Use Initialization from BaseClass
+	public override void Awake () {
+		base.Awake ();
+		toggleGroup = GetComponent<ToggleGroup> ();
+
+	}
+
+
+
+}
