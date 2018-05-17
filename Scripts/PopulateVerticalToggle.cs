3e16618aea4f54032efe8d9c7d3a7c4e14999c64
ukirsche <ukirschenmann@gmx.de>
2018-05-17 14:31:45 +0200

    Issue #40: Refactoring PopulateVertical

--- Scripts/PopulateVerticalToggle.cs
+++ Scripts/PopulateVerticalToggle.cs
@@ -5,16 +5,39 @@ using UnityEngine.UI;
 
 public class PopulateVerticalToggle : PopulateVertical {
 
-	private ToggleGroup toggleGroup;
-	private List<Toggle> toggleButtons;
+	public GameObject prefabDialogToggle;
+	public ToggleGroup toggleGroup;
+	private List<GameObject> dialogToggleElements;
+
 
 	// Use Initialization from BaseClass
 	public override void Awake () {
 		base.Awake ();
+		dialogToggleElements = new List<GameObject> ();
 		toggleGroup = GetComponent<ToggleGroup> ();
-
 	}
 
 
+	/// <summary>
+	/// Clears the dialg text.
+	/// </summary>
+	public override void ClearDialogBox(){
+		if (dialogToggleElements.Count > 0) {
+			foreach (var item in dialogToggleElements) {
+				Destroy (item);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Adds the dialog option to the Vertical Group
+	/// </summary>
+	public void addDialogOption(string dialogString){
+		GameObject newDialog;
+		newDialog = Instantiate (prefabDialogToggle, transform); //ensures that
+		newDialog.GetComponent<Toggle>().group = toggleGroup;
+		newDialog.GetComponentInChildren<Text> ().text = dialogString;
+		dialogToggleElements.Add (newDialog);
+	}
 
 }
