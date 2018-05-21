f63353b5136d9ca1dafdfa7c129de27655d5749f
ukirsche <ukirschenmann@gmx.de>
2018-05-21 11:29:57 +0200

    Issue #47: Refactoring statische Methoden aus ButtonWrapper nach DisplayManager

--- Scripts/Dialog/DialogDisplayManager.cs
+++ Scripts/Dialog/DialogDisplayManager.cs
@@ -13,4 +13,33 @@ public class DialogDisplayManager : MonoBehaviour {
 	void Update () {
 		
 	}
+
+	/// <summary>
+	/// Displays a simple Dialog in Rows
+	/// </summary>
+	/// <param name="infos">Infos.</param>
+	public static void DisplayDialogText(List<string> dialogRows, PopulateVertical populateDialog){
+		populateDialog.ClearDialogBox ();
+		if (dialogRows.Count > 0) {
+			foreach (var dialogRow in dialogRows) {
+				populateDialog.addDialogText (dialogRow);
+
+			}
+		} 
+	}
+
+	/// <summary>
+	/// Displays a Dialog Page 
+	/// </summary>
+	/// <param name="infos">Infos.</param>
+	public static void DisplayDialogOption(List<string> dialogRows, PopulateVertical populateDialog){
+		PopulateVerticalToggle popVertical = populateDialog as PopulateVerticalToggle;
+		popVertical.ClearDialogBox ();
+		if (dialogRows.Count > 0) {
+			foreach (var dialogRow in dialogRows) {
+				popVertical.addDialogOption(dialogRow);
+
+			}
+		} 
+	}
 }
