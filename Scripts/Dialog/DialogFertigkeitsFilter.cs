db61dd33415d5fbe19626a0aa00d507618f03d8a
ukirsche <ukirschenmann@gmx.de>
2018-05-24 10:46:35 +0200

    Issue #39: Klassen erweitert für Filter

---
+++ Scripts/Dialog/DialogFertigkeitsFilter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Filtert die Dialog gemäß den Fertigkeiten
+/// </summary>
+public class DialogFertigkeitsFilter {
+
+	private DialogParser dialogParser;
+	public bool IsFilterDialog { get; set;}
+
+
+	public DialogFertigkeitsFilter(DialogParser _dialogParser){
+
+		dialogParser = _dialogParser;
+		IsFilterDialog = false;
+	}
+
+
+	public List<string> FilterDialog(){
+		if (IsFilterDialog) {
+			
+
+		} else {
+			return dialogParser.GetInfos ();
+		}
+	}
+
+}
